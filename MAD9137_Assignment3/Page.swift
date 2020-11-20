75f79f5f9925b60319b03762154fa65b02daaf42
RaviC Rachamalla <rach0022@algonquinlive.com>
2020-11-20 15:35:42 -0500

    commented code ready for submission

--- MAD9137_Assignment3/Page.swift
+++ MAD9137_Assignment3/Page.swift
@@ -14,55 +14,15 @@ class Page {
     // A page number int that will reference this objects page number
 //    var _pageNumber: Int?
     var pageNumber: Int?
-//    {
-//        get {
-//            return _pageNumber
-//        }
-//        set {
-//            guard newValue! >= 0 else {
-//                print("Page number must not be negative")
-//                return
-//            }
-//            self._pageNumber = newValue
-//        }
-//    }
     
     // pageText will hold the text for the page
     var pageText: String?
-//    {
-//        get {
-//            if let text = self.pageText{
-//                return text
-//            }
-//            return ""
-//        }
-//        set {
-//            guard newValue == "" else {
-//                print("String text must not be empty")
-//                return
-//            }
-//            self.pageText = newValue
-//        }
-//    }
+
     // pageChoiceA and pageChoiceB will be used to reference the 2 possible paths
     // for the user
     var pageChoiceA: Page?
-    
-//    var pageA: Page
-//    {
-//        get {
-//            // check to see if the pageChoiceA exists first then return it
-//            if let page = self.pageChoiceA {
-//                return page
-//            } else {
-//                return nil
-//            }
-//        }
-//        set {
-//            self.pageChoiceA = newValue
-//        }
-//    }
     var pageChoiceB: Page?
+    
     // Custom properties for the Page class
     // pageColour will be used to determine the background colour of the viewController
     var pageColour: UIColor?
@@ -76,19 +36,20 @@ class Page {
         }
     }
     
-    
     // the default init will only need the page number and pageText
     init(_pageNumber: Int, _pageText: String) {
         self.pageNumber = _pageNumber
         self.pageText = _pageText
     }
     
+    // convenience init that sets the page choices and base init, currently not used
     convenience init(_pageNumber: Int, _pageText: String, _pageChoiceA: Page, _pageChoiceB: Page) {
         self.init(_pageNumber: _pageNumber, _pageText: _pageText)
         self.pageChoiceA = _pageChoiceA
         self.pageChoiceB = _pageChoiceB
     }
     
+    // convenience init that sets the themeString along with the base init, currently not used
     convenience init(_pageNumber: Int, _pageText: String, _themeString: String) {
         self.init(_pageNumber: _pageNumber, _pageText: _pageText)
         setTheme(themeString: _themeString)
@@ -142,3 +103,47 @@ class Page {
         self._pageBannerText = themeArray[1]
     }
 }
+
+
+// *** UNCOMMENTED CODE: *****
+//    {
+//        get {
+//            if let text = self.pageText{
+//                return text
+//            }
+//            return ""
+//        }
+//        set {
+//            guard newValue == "" else {
+//                print("String text must not be empty")
+//                return
+//            }
+//            self.pageText = newValue
+//        }
+//    }
+//    var pageA: Page
+//    {
+//        get {
+//            // check to see if the pageChoiceA exists first then return it
+//            if let page = self.pageChoiceA {
+//                return page
+//            } else {
+//                return nil
+//            }
+//        }
+//        set {
+//            self.pageChoiceA = newValue
+//        }
+//    }
+//    {
+//        get {
+//            return _pageNumber
+//        }
+//        set {
+//            guard newValue! >= 0 else {
+//                print("Page number must not be negative")
+//                return
+//            }
+//            self._pageNumber = newValue
+//        }
+//    }
